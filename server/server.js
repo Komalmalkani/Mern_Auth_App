511d8547997e55cc7c502f6329dce6d3741d843d
Komal Malkani <malkanianita15@gmail.com>
2024-09-24 16:26:40 +0530

    commit all changes

--- server/server.js
+++ server/server.js
@@ -2,15 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const authRouter = require("./router/auth-router")
-const contactRoute = require("./router/contact-router")
 const connectDB = require("../server/utils/db")
-const errorMiddleware = require("../server/middlewares/error-middleware")
 
 app.use(express.json());
 app.use("/api/auth",authRouter);
-app.use("/api/form",contactRoute)
-app.use(errorMiddleware);
 
+ 
 const PORT = 5000
 
 connectDB().then(()=>{
