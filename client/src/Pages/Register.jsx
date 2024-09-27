317ab5da9eb89c612ce4090d63a0a2299ce0e793
Komal Malkani <malkanianita15@gmail.com>
2024-09-27 12:27:36 +0530

    commiting some new changes

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -2,7 +2,51 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+   <>
+    <section>
+      <main>
+        <div className='section-registration'>
+        <div className='container grid grid-two-cols'>
+        <div className='registration-image'>
+        <img src='/images/register.png' alt='a girl is trying to do registration' width="200" height="200"/>
+        </div>
+
+        {/* let's tackle registration form */}
+        <div className='registration-form'>
+          <h1 className='main-heading mb-3'>Registeration Form</h1>
+          <br/>
+          <form>
+            <div>
+              <label htmlFor='username'>
+              Username
+              </label>
+              <input type='text' name='username' placeholder='username' id='username' required autoComplete='off'/>
+             <br/>
+              <label htmlFor='Email'>
+              Email 
+              </label>
+              <input type='Email' name='email' placeholder='Enter your Email' id='Email' required autoComplete='off'/>
+            <br/>
+              <label htmlFor='phone'>
+              phone
+              </label>
+              <input type='Number' name='phone' placeholder='phone' id='phone' required autoComplete='off'/>
+              <br/>
+              <label htmlFor='Password'>
+              Password
+              </label>
+              <input type='password' name='Password' placeholder='Password' id='Password' required autoComplete='off'/>
+            </div>
+            <br/>
+            <button type='submit' className='btn'>Register Now</button>
+          </form>
+        </div>
+       
+        </div>
+        </div>
+      </main>
+    </section>
+   </>
   )
 }
 
