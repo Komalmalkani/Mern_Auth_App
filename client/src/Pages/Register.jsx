b860d922c035403e20a376dedfd86c28fe6cb3f9
Komal Malkani <malkanianita15@gmail.com>
2024-10-06 17:47:09 +0530

    Registration page

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -3,6 +3,22 @@ import React from 'react'
 const Register = () => {
   return (
    <>
+   <section>
+    <main>
+      <div className='section-registeration'>
+      <div className='container grid grid-two-cols'>
+      <div className='registeration-image'>
+      <img src='/images/register.png' alt='a girl is trying to do registeration'
+        width="400"
+      />
+
+      </div>
+
+      </div>
+
+      </div>
+    </main>
+   </section>
 
    </>
   )
